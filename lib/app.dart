9d21f2bb7c2a23321bb2663d73a3db1365fc41dc
nirmalnyure1 <nirmalnyure1@gmail.com>
2024-01-01 17:03:02 +0545

    initial changes

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    ));
+  }
+}
